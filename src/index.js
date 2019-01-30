58b08a1a9d5a046b21c56f0825145bd2b7c47153
Mariel Martinez <mcortez.martinez@gmail.com>
2019-01-29 21:47:29 -0500

    Completed 04-04

--- src/index.js
+++ src/index.js
@@ -20,20 +20,52 @@ const Book = ({title, author, pages}) => {
 }
 
 // Parent component which renders Book components
-const Library = ({books}) => {
-    return (
-        <div>
-            {/* We're mapping all the books in the array */}
-            {/* No need to use braces with ES6 if the arrow points to whatever you're returning */}
-            {books.map(
-                (book, i) => 
-                    <Book key={i}
-                          title={book.title}
-                          author={book.author} 
-                          pages={book.pages}/>
-            )}
-        </div>
-    )
+class Library extends Component {
+    
+    // Allows us to get rid of the constructor
+    state = { open: false } // Static property
+    
+    // constructor(props) {
+    //     super(props)
+    //     // State is just an object with several keys, similar to props
+    //     // Can use these values within the context of the Library component
+    //     this.state = {
+    //         open: true
+    //     }
+
+    //     // Binding
+    //     // Makes "this" accessible with the context of custom methods
+    //     this.toggleOpenClosed = this.toggleOpenClosed.bind(this)
+    // }
+
+    // Will trigger change of state
+    // Is asynchronous
+    toggleOpenClosed = () => { // automatically binds this in context of function
+        // Takes in object that resets the state
+        this.setState(prevState => ({ // callback function
+            open: !prevState.open
+        }))
+    }
+
+    render() {
+        console.log(this.state)
+        const {books} = this.props;
+        return (
+            <div>
+                <h1>The Library is {this.state.open ? 'Open' : 'Closed'}</h1>
+                <button onClick={this.toggleOpenClosed}>Change</button>
+                {/* We're mapping all the books in the array */}
+                {/* No need to use braces with ES6 if the arrow points to what you're returning */}
+                {books.map(
+                    (book, i) => 
+                        <Book key={i}
+                              title={book.title}
+                              author={book.author} 
+                              pages={book.pages}/>
+                )}
+            </div>
+        )
+    }
 }
 
 render( // ReactDOM.render()
