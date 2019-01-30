812424a319bf51ff9cbe441e65223f1d07b3b2ce
Mariel Martinez <mcortez.martinez@gmail.com>
2019-01-29 21:29:34 -0500

    Completed 04-02

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'; // Destructured
 import { render } from 'react-dom'
 
+// Acts like a database, could be a json file
+let bookList = [
+    {"title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260},
+    {"title": "White Teeth", "author": "Zadie Smith", "pages": 480},
+    {"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304},
+];
+
 // Child component
 const Book = ({title, author, pages}) => {
     return (
@@ -13,25 +20,25 @@ const Book = ({title, author, pages}) => {
 }
 
 // Parent component which renders Book components
-const Library = () => {
+const Library = ({books}) => {
     return (
         <div>
-            <Book title="The Sun Also Rises" 
-                  author="Ernest Hemingway" 
-                  pages={260}/>
-            <Book title="White Teeth" 
-                  author="Zadie Smith" 
-                  pages={480}/>
-            <Book title="Cat's Cradle" 
-                  author="Kurt Vonnegut" 
-                  pages={304}/>
+            {/* We're mapping all the books in the array */}
+            {/* No need to use braces with ES6 if the arrow points to whatever you're returning */}
+            {books.map(
+                (book, i) => 
+                    <Book key={i}
+                          title={book.title}
+                          author={book.author} 
+                          pages={book.pages}/>
+            )}
         </div>
     )
 }
 
 render( // ReactDOM.render()
     // What we want to render
-    <Library />,
+    <Library books={bookList} />,
 
     // Where do we want to render this? ... to root
     document.getElementById('root') 
